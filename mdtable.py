c8746b67c5ebb467ed9e30dc5a607469217d4b3e
Giovanni Giglio <giovannimaria.giglio@gmail.com>
2018-04-09 22:28:39 +0200

    ADD: -f option and more checks

--- mdtable.py
+++ mdtable.py
@@ -1,18 +1,29 @@
 #!/usr/bin/python
+#encoding: utf-8
 from __future__ import print_function
 import sys
 
 def main():
 	cols = len(sys.argv) -1
-	if not cols > 1:
-		print('Invalid format: more than 1 cols needed', file=sys.stderr)
-		return
 
-	print(*sys.argv[1:],sep=' | ')
-	print((cols * ' --- |').strip('|'))
+	if len(sys.argv) < 2:
+		print("Error: Provide column names or '-f' flag to get them from the first line", file=sys.stderr)
+		exit(1)
+
+	# check -f flag
+	if sys.argv[1] == '-f':
+		headers = next(sys.stdin).split()
+		print(*headers, sep=' | ')
+		cols = len(headers)
+	elif cols < 2:
+		print('Error: More than 1 column needed', file=sys.stderr)
+		exit(1)
+	else:
+		print(*sys.argv[1:],sep=' | ')
+	
+	print((cols * ' --- |').strip('|').strip())
 
 	for line in sys.stdin:
-		line = line.strip()
 		tokens = line.split()
 		tokens[cols-1] = ' '.join((tokens[cols -1:]))
 		print(*tokens[0:cols], sep=' | ')
